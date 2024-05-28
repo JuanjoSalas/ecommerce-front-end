d97ef1be83ac9ee87d380418d0682f9065ed3c12
juan salas <juanjo.salas.dev@gmail.com>
2024-05-28 12:23:48 +0200

    spin

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect} from 'react';
 import { ProductContext } from "../../context/ProductContext/ProductState";
+import { Spin } from "antd";
 
 const Products = () => {
   const { products, getProducts } = useContext(ProductContext);
@@ -8,17 +9,17 @@ const Products = () => {
   }, []);
 
   if(!products.games){
-    return <p>Cargando...</p>
+    return <Spin />;
   }
   const product = products.games.map((product) => {
       return (
         <div key={product.id}>
+          <img src={product.image}/>
           <p>{product.name}</p>
           <p> {product.price}€</p>
           <span>{product.players}</span>
           <span>+{product.age}</span>
         </div>
-        
       )
   })
 
