8953a38ffc6d5d1e1b3e53541d8e39b771244cdb
juan salas <juanjo.salas.dev@gmail.com>
2024-06-02 12:57:38 +0200

    register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { useContext } from "react";
 import { UserContext } from "../../context/UserContext/UserState";
+import "./Register.scss";
 
 const Register = ()=>{
     const {register} = useContext(UserContext)
@@ -21,12 +22,15 @@ const Register = ()=>{
         console.log('formData',formData)
     }
   return (
-    <form onSubmit={onSubmit}>
-        <input type="text" name="name" placeholder="Nombre y Apellidos" required value={name} onChange={onChange} />
-        <input type="email" name="email" placeholder="Correo electrónico" required value={email} onChange={onChange}/>
-        <input type="password" name="password" placeholder="Contraseña" required value={password} onChange={onChange}/>
-        <button type="submit">Register</button>
+    <div className="Register-container">
+    <form class="form-register" onSubmit={onSubmit}>
+        <input class="infos" id="nome" type="text" name="name" placeholder="Nombre y Apellidos" required value={name} onChange={onChange} />
+        <div class="mario"></div>
+        <input id="email" type="email" name="email" placeholder="Correo electrónico" required value={email} onChange={onChange}/>
+        <input class="password" type="password" name="password" placeholder="Contraseña" required value={password} onChange={onChange}/>
+        <button class="btn-register" type="submit">Register</button>
     </form>
+    </div>
   )
 }
 export default Register
